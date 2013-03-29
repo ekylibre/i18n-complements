45a07fb28634cba3cf5337b52bc9303047547a42
Brice Texier <burisu@oneiros.fr>
2013-03-29 07:19:55 +0100

    Changes Coveralls settings

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,8 @@
+require 'coveralls'
+Coveralls.wear!
+
 require 'bundler/setup'
 require 'test/unit'
-
-require 'coveralls'
-Coveralls.wear!('rails')
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
